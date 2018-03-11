93110856d2ea0f3a3be564dfa9249db843b2d066
Steven McGrath <mcgrath.steven7@gmail.com>
2018-03-11 19:06:13 -0400

    First commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const path    = require('path'),
 			express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-	console.log('Now listening on port 3000');
+app.listen(port, () => {
+	console.log(`Now listening on port ${port}`);
 });
