5b35f15ffdf7ae6416cacc200764df4467c5bd9b
Steven McGrath <mcgrath.steven7@gmail.com>
2018-03-11 20:15:37 -0400

    Testing with socket io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
-const path    = require('path'),
-			express = require('express');
+const path     = require('path'),
+			express  = require('express'),
+			http     = require('http'),
+			socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
 
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+	socket.on('disconnect', (reason) => {
+		console.log('Client disconnected. Reason:', reason);
+	});
+});
+
+app.get('/', (req, res) => {
+	res.sendfile(publicPath);
+});
+
+io.on('connection', (socket) => {
+	socket.emit('news', {hello: 'world'});
+	socket.on('my other event', (data) => {
+		console.log(data);
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Now listening on port ${port}`);
 });
