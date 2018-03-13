d22662f2962482190d4f82330d67253f01aa6a54
Steven McGrath <mcgrath.steven7@gmail.com>
2018-03-13 16:02:05 -0400

    Added User class methods - addUser, getUser, removeUser, getUserList

--- server/utils/users.js
+++ server/utils/users.js
@@ -10,6 +10,24 @@ class Users {
 		return user;
 	}
 	
+	removeUser (id) {
+		let user = this.getUser(id);
+		if (user) {
+			this.users = this.users.filter((user) => user.id !== id);
+		}
+		return user;
+	}
+	
+	getUser (id) {
+		return this.users.filter((user) => user.id === id)[0];
+	}
+	
+	getUserList (room) {
+		let users = this.users.filter((user) => user.room === room);
+		return users.map((user) => user.name);
+	};
+	
+	
 }
 
 module.exports = {Users};
